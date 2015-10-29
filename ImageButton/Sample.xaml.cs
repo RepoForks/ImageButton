f67aea7ff82f33eb9d0fd438f96a3cd4e00f7bee
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2015-10-29 16:42:11 +0400

    Initial commit

---
+++ ImageButton/Sample.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImageButton
+{
+	public partial class Sample : ContentPage
+	{
+		public Sample()
+		{
+			this.BindingContext = new SampleViewModel();
+			InitializeComponent();
+		}
+	}
+}
+
---
+++ ImageButton/SampleViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace ImageButton
+{
+	public class SampleViewModel : INotifyPropertyChanged
+	{
+		public SampleViewModel()
+		{
+		}
+
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		private bool _isSelected;
+
+		public bool IsSelected
+		{ 
+			get
+			{
+				return _isSelected; 
+			}
+			set
+			{
+				_isSelected = value; 
+				RaisePropertyChanged("IsSelected"); 
+			}
+		}
+
+		private bool _isLoading;
+
+		public bool IsLoading
+		{ 
+			get
+			{
+				return _isLoading; 
+			}
+			set
+			{
+				_isLoading = value; 
+				RaisePropertyChanged("IsLoading"); 
+				RaisePropertyChanged("CanExecuteToggleFavoriteCommand"); 
+			}
+		}
+
+		public bool CanExecuteToggleFavoriteCommand
+		{
+			get
+			{
+				return!this.IsLoading;
+			}
+		}
+
+
+
+		private Command _toggleFavoriteCommand;
+
+		public System.Windows.Input.ICommand ToggleFavoriteCommand
+		{
+			get
+			{
+				_toggleFavoriteCommand = _toggleFavoriteCommand ?? new Command(async () => {
+					IsLoading = true;
+					await Task.Delay(1000);
+					this.IsSelected = !IsSelected;
+					IsLoading = false;
+				}, () => !IsLoading);
+				return _toggleFavoriteCommand;
+			}
+		}
+
+		void RaisePropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
+
